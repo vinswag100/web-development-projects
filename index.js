4f3fb75f7a96b4db0e562611f5aed06ac216e19b
vincent ogieva <91390530+vinswag100@users.noreply.github.com>
2024-10-25 13:59:38 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("hello from node.js");
+/* 
+1. Use the inquirer npm package to get user input.
+2. Use the qr-image npm package to turn the user entered URL into a QR code image.
+3. Create a txt file to save the user input using the native fs node module.
+*/
